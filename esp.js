6cdf9c4b783396f6d424f71a78d8b2c1c19b7f43
Fredrik Liljegren <fredrik.liljegren@textalk.se>
2016-06-10 17:41:01 +0200

    Bugfix: bad query

--- esp.js
+++ esp.js
@@ -180,7 +180,7 @@ EspPrototype.subscribeToStreamFrom = function(streamId, from, params) {
       let eventNumber = -1
       return Promise.all([
         subStream,
-        new Promise((resolve, reject) => this.readStreamEventsForward(from, options)
+        new Promise((resolve, reject) => this.readStreamEventsForward(streamId, from, options)
           .on('error', err   => reject(err))
           .on('end',   ()    => resolve(eventNumber))
           .on('data',  event => {
